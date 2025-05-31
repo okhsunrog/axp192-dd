d4e9e54164ab8b5ba728b21a363ce350b14b65a1
Danila Gornushko <me@okhsunrog.dev>
2025-06-01 01:00:12 +0300

    Improving yaml (#1)
    * creating new yaml
    
    * ChargeStatus added
    
    * OtgVbusStatus added
    
    * added DataBuffers
    
    * added ExtenDcDc2Control
    
    * added PowerOutputControl
    
    * added DcDc2VoltageSetting
    
    * added DcDc2VrcParameter
    
    * added DcDc1VoltageSetting
    
    * added DcDc3VoltageSetting and DcDc2VoltageSetting
    
    * make it ref
    
    * added Ldo2And3VoltageSetting
    
    * added VbusIpsoutPathManagement
    
    * improve reset values
    
    * added ShutdownVoltageSetting
    
    * added ShutdownBatChgLedControl
    
    * added ChargeControl1
    
    * renaming
    
    * migrate to using conversion
    
    * nitpicking
    
    * added ChargeControl2
    
    * added thresholds group + ntc
    
    * added DcDcOperatingMode
    
    * added ADC settings
    
    * gpio1 irq config
    
    * adding gpio setting
    
    * adding pwm setting
    
    * added NrstoGpio5Control
    
    * added IRQ registers
    
    * added Interrupt Status Registers
    
    * adding adc registers
    
    * adding adc registers x2
    
    * added BatteryInstantaneousPowerAdc
    
    * finished the adc section
    
    * added CoulombCounter

--- src/driver_core.rs
+++ src/driver_core.rs
@@ -1,12 +1,22 @@
-use super::*; // This imports from lib.rs in the context of either `asynchronous` or `blocking` module
-// It will bring CurrentAxpDriverInterface into scope.
+use super::*;
 use crate::{
-    AxpError, ChargeTargetVoltage, DcId, GpioMode, GpioPin, LdoId, PekBootTime, PekLongPressTime,
+    AxpError,
+    ChargeTargetVoltage, // Using your existing ChargeTargetVoltage
+    DcId,
+    GpioMode,
+    GpioPin,
+    LdoId,
+    PekBootTime,
+    PekLongPressTime,
     PekShutdownDuration,
+    // Add other enums you created for high-level API as needed:
+    // Gpio0ModeSetting, BackupBatteryTargetVoltageSetting, BackupBatteryChargeCurrentSetting,
+    // ChgLedPinFunctionSetting, NOeShutdownDelaySetting,
 };
 
 device_driver::create_device!(device_name: Device, manifest: "device.yaml");
 
+// --- AxpInterface and its impls remain the same ---
 pub struct AxpInterface<I2CBus> {
     i2c_bus: I2CBus,
     device_address: u8,
@@ -44,12 +54,9 @@ where
         data: &[u8],
     ) -> Result<(), Self::Error> {
         if data.len() > 2 {
-            // AXP192 registers are typically 1 byte. Some writes might involve 2 bytes for ADC settings.
-            return Err(AxpError::NotImplemented(
-                "Write data potentially too large for typical AXP192 registers, check datasheet.",
-            ));
+            return Err(AxpError::NotImplemented("Write data too large"));
         }
-        let mut buffer = [0u8; 1 + 2]; // Max 1 address byte + 2 data bytes
+        let mut buffer = [0u8; 1 + 2];
         buffer[0] = address;
         buffer[1..1 + data.len()].copy_from_slice(data);
         self.i2c_bus
@@ -83,9 +90,7 @@ where
         data: &[u8],
     ) -> Result<(), Self::Error> {
         if data.len() > 2 {
-            return Err(AxpError::NotImplemented(
-                "Write data potentially too large for typical AXP192 registers, check datasheet.",
-            ));
+            return Err(AxpError::NotImplemented("Write data too large"));
         }
         let mut buffer = [0u8; 1 + 2];
         buffer[0] = address;
@@ -128,10 +133,8 @@ where
     }
 }
 
-// This impl block will be processed by `bisync` for both sync and async versions
 impl<I2CImpl, I2CBusErr> Axp192<I2CImpl>
 where
-    // Use the aliased trait here
     I2CImpl: CurrentAxpDriverInterface<AddressType = u8, Error = AxpError<I2CBusErr>>,
     I2CBusErr: core::fmt::Debug,
 {
@@ -141,13 +144,13 @@ where
         dc: DcId,
         enable: bool,
     ) -> Result<(), AxpError<I2CBusErr>> {
-        let mut op = self.ll.dcdc_13_ldo_23_control();
-        op.modify(|r| match dc {
-            DcId::Dcdc1 => r.set_dcdc_1_enable(enable),
-            DcId::Dcdc3 => r.set_dcdc_3_enable(enable),
-        })
-        .await?;
-        Ok(())
+        self.ll
+            .dcdc_13_ldo_23_control()
+            .modify(|r| match dc {
+                DcId::Dcdc1 => r.set_dcdc_1_enable(enable), // Corrected: dcdc_1_enable
+                DcId::Dcdc3 => r.set_dcdc_3_enable(enable), // Corrected: dcdc_3_enable
+            })
+            .await
     }
 
     #[bisync(async_suffix = "async")]
@@ -159,18 +162,18 @@ where
         if !(700..=3500).contains(&voltage_mv) {
             return Err(AxpError::InvalidVoltage(voltage_mv));
         }
-        let setting = ((voltage_mv - 700) / 25) as u8 & 0x7F;
+        let setting = ((voltage_mv - 700) / 25) as u8;
         match dc {
             DcId::Dcdc1 => {
                 self.ll
                     .dcdc_1_voltage()
-                    .modify(|r| r.set_setting(setting))
+                    .modify(|r| r.set_setting(setting & 0x7F))
                     .await?;
             }
             DcId::Dcdc3 => {
                 self.ll
                     .dcdc_3_voltage()
-                    .modify(|r| r.set_setting(setting))
+                    .modify(|r| r.set_setting(setting & 0x7F))
                     .await?;
             }
         }
@@ -186,7 +189,7 @@ where
     #[bisync(async_suffix = "async")]
     pub async fn get_battery_voltage_mv(&mut self) -> Result<f32, AxpError<I2CBusErr>> {
         let fieldset = self.ll.battery_voltage_adc().read().await?;
-        Ok((fieldset.raw() >> 4) as f32 * 1.1)
+        Ok(fieldset.raw() as f32 * 1.1)
     }
 
     #[bisync(async_suffix = "async")]
@@ -195,13 +198,13 @@ where
         ldo: LdoId,
         enable: bool,
     ) -> Result<(), AxpError<I2CBusErr>> {
-        let mut op = self.ll.dcdc_13_ldo_23_control();
-        op.modify(|r| match ldo {
-            LdoId::Ldo2 => r.set_ldo_2_enable(enable),
-            LdoId::Ldo3 => r.set_ldo_3_enable(enable),
-        })
-        .await?;
-        Ok(())
+        self.ll
+            .dcdc_13_ldo_23_control()
+            .modify(|r| match ldo {
+                LdoId::Ldo2 => r.set_ldo_2_enable(enable), // Corrected: ldo_2_enable
+                LdoId::Ldo3 => r.set_ldo_3_enable(enable), // Corrected: ldo_3_enable
+            })
+            .await
     }
 
     #[bisync(async_suffix = "async")]
@@ -214,13 +217,13 @@ where
             return Err(AxpError::InvalidVoltage(voltage_mv));
         }
         let setting = ((voltage_mv - 1800) / 100) as u8 & 0x0F;
-        let mut op = self.ll.ldo_23_voltage();
-        op.modify(|r| match ldo {
-            LdoId::Ldo2 => r.set_ldo_2_setting(setting),
-            LdoId::Ldo3 => r.set_ldo_3_setting(setting),
-        })
-        .await?;
-        Ok(())
+        self.ll
+            .ldo_23_voltage()
+            .modify(|r| match ldo {
+                LdoId::Ldo2 => r.set_ldo_2_setting(setting), // Corrected: ldo_2_setting
+                LdoId::Ldo3 => r.set_ldo_3_setting(setting), // Corrected: ldo_3_setting
+            })
+            .await
     }
 
     #[bisync(async_suffix = "async")]
@@ -232,29 +235,39 @@ where
             0..=100 => 0b0000,
             101..=190 => 0b0001,
             191..=280 => 0b0010,
-            281..=370 => 0b0011,
-            371..=460 => 0b0100,
-            461..=550 => 0b0101,
-            551..=640 => 0b0110,
-            641..=780 => 0b0111,
+            281..=360 => 0b0011,
+            361..=450 => 0b0100,
+            451..=550 => 0b0101,
+            551..=630 => 0b0110,
+            631..=700 => 0b0111,
+            701..=780 => 0b1000,
+            781..=880 => 0b1001,
+            881..=960 => 0b1010,
+            961..=1000 => 0b1011,
+            1001..=1080 => 0b1100,
+            1081..=1160 => 0b1101,
+            1161..=1240 => 0b1110,
+            1241..=1320 => 0b1111,
             _ => return Err(AxpError::InvalidCurrent(current_ma)),
         };
-        let mut op = self.ll.charge_control_1();
-        op.modify(|r| r.set_charge_current_setting(current_bits))
-            .await?;
-        Ok(())
+        self.ll
+            .charge_control_1()
+            .modify(|r| {
+                r.set_charge_current_setting(current_bits);
+            })
+            .await
     }
 
     #[bisync(async_suffix = "async")]
     pub async fn get_acin_voltage_mv(&mut self) -> Result<f32, AxpError<I2CBusErr>> {
         let fieldset = self.ll.acin_voltage_adc().read().await?;
-        Ok((fieldset.raw() >> 4) as f32 * 1.7)
+        Ok(fieldset.raw() as f32 * 1.7)
     }
 
     #[bisync(async_suffix = "async")]
     pub async fn get_internal_temperature_c(&mut self) -> Result<f32, AxpError<I2CBusErr>> {
         let fieldset = self.ll.internal_temperature_adc().read().await?;
-        Ok(((fieldset.raw() >> 4) as f32 * 0.1) - 144.7)
+        Ok((fieldset.raw() as f32 * 0.1) - 144.7)
     }
 
     #[bisync(async_suffix = "async")]
@@ -269,19 +282,19 @@ where
                 self.ll
                     .gpio_0_control()
                     .modify(|r| r.set_mode(mode_val))
-                    .await?
+                    .await?;
             }
             GpioPin::Gpio1 => {
                 self.ll
                     .gpio_1_control()
                     .modify(|r| r.set_mode(mode_val))
-                    .await?
+                    .await?;
             }
             GpioPin::Gpio2 => {
                 self.ll
                     .gpio_2_control()
                     .modify(|r| r.set_mode(mode_val))
-                    .await?
+                    .await?;
             }
         };
         Ok(())
@@ -293,13 +306,26 @@ where
         pin: GpioPin,
         level_high: bool,
     ) -> Result<(), AxpError<I2CBusErr>> {
-        let mut op = self.ll.gpio_20_signal_status();
-        op.modify(|r| match pin {
-            GpioPin::Gpio0 => r.set_gpio_0_out_level(level_high),
-            GpioPin::Gpio1 => r.set_gpio_1_out_level(level_high),
-            GpioPin::Gpio2 => r.set_gpio_2_out_level(level_high),
-        })
-        .await?;
+        match pin {
+            GpioPin::Gpio0 => {
+                self.ll
+                    .gpio_20_signal_status()
+                    .modify(|r| r.set_gpio_0_output_level_setting(level_high))
+                    .await?; // Corrected
+            }
+            GpioPin::Gpio1 => {
+                self.ll
+                    .gpio_20_signal_status()
+                    .modify(|r| r.set_gpio_1_output_level_setting(level_high))
+                    .await?; // Corrected
+            }
+            GpioPin::Gpio2 => {
+                self.ll
+                    .gpio_20_signal_status()
+                    .modify(|r| r.set_gpio_2_output_level_setting(level_high))
+                    .await?; // Corrected
+            }
+        };
         Ok(())
     }
 
@@ -308,25 +334,224 @@ where
         &mut self,
         boot_time: PekBootTime,
         long_press: PekLongPressTime,
-        auto_shutdown_by_pwrok_en: bool,
-        pwrok_signal_delay_64ms: bool,
+        auto_shutdown_on_long_press: bool, // Changed name to match YAML
+        pwrok_delay_64ms: bool,
         shutdown_duration: PekShutdownDuration,
     ) -> Result<(), AxpError<I2CBusErr>> {
-        let mut op = self.ll.pek_settings();
-        let pwrok_delay_val_for_reg = if pwrok_signal_delay_64ms { 1 } else { 0 };
-        op.write(|w| {
-            w.set_boot_time_setting(boot_time as u8);
-            w.set_long_press_time_setting(long_press as u8);
-            w.set_auto_shutdown_by_pwrok_en(auto_shutdown_by_pwrok_en);
-            w.set_pwrok_signal_delay(pwrok_delay_val_for_reg);
-            w.set_shutdown_duration_setting(shutdown_duration as u8);
-        })
-        .await?;
-        Ok(())
+        let pwrok_delay_val_for_reg = if pwrok_delay_64ms { 1 } else { 0 }; // Assuming 1-bit field
+        self.ll
+            .pek_settings()
+            .write(|w| {
+                w.set_boot_time_setting(boot_time as u8);
+                w.set_long_press_time_setting(long_press as u8);
+                w.set_auto_shutdown_on_long_press_enable(auto_shutdown_on_long_press); // Corrected
+                w.set_pwrok_signal_delay_after_power_on(pwrok_delay_val_for_reg); // Corrected
+                w.set_shutdown_duration_setting(shutdown_duration as u8);
+            })
+            .await
     }
 
     #[bisync]
     pub async fn set_led_enable(&mut self, enable: bool) -> Result<(), AxpError<I2CBusErr>> {
         self.set_gpio_output_level(GpioPin::Gpio1, !enable).await
+    }
+
+    // --- NEW HIGH-LEVEL API METHODS FOR M5StickC-Plus init ---
+
+    #[bisync(async_suffix = "async")]
+    pub async fn enable_all_adcs_reg1(&mut self) -> Result<(), AxpError<I2CBusErr>> {
+        // Renamed from enable_all_adcs
+        self.ll
+            .adc_enable_1()
+            .write(|r| {
+                r.set_battery_voltage_adc_enable(true);
+                r.set_battery_current_adc_enable(true);
+                r.set_acin_voltage_adc_enable(true);
+                r.set_acin_current_adc_enable(true);
+                r.set_vbus_voltage_adc_enable(true);
+                r.set_vbus_current_adc_enable(true);
+                r.set_aps_voltage_adc_enable(true);
+                r.set_ts_pin_adc_enable(true);
+            })
+            .await
+    }
+
+    #[bisync(async_suffix = "async")]
+    pub async fn enable_all_adcs_reg2(&mut self) -> Result<(), AxpError<I2CBusErr>> {
+        self.ll
+            .adc_enable_2()
+            .write(|r| {
+                r.set_internal_temp_adc_enable(true);
+                r.set_gpio_0_adc_enable(true); // Corrected
+                r.set_gpio_1_adc_enable(true); // Corrected
+                r.set_gpio_2_adc_enable(true); // Corrected
+                r.set_gpio_3_adc_enable(true); // Corrected
+            })
+            .await
+    }
+
+    #[bisync(async_suffix = "async")]
+    pub async fn set_charge_control_basic(
+        // Renamed from set_charging_parameters
+        &mut self,
+        enable_charging: bool,
+        target_voltage: ChargeTargetVoltage, // Using your enum from lib.rs
+        end_current_is_15_percent: bool,
+        current_ma: u16,
+    ) -> Result<(), AxpError<I2CBusErr>> {
+        let current_bits = match current_ma {
+            0..=100 => 0b0000,
+            101..=190 => 0b0001,
+            191..=280 => 0b0010,
+            281..=360 => 0b0011,
+            361..=450 => 0b0100,
+            451..=550 => 0b0101,
+            551..=630 => 0b0110,
+            631..=700 => 0b0111,
+            701..=780 => 0b1000,
+            781..=880 => 0b1001,
+            881..=960 => 0b1010,
+            961..=1000 => 0b1011,
+            1001..=1080 => 0b1100,
+            1081..=1160 => 0b1101,
+            1161..=1240 => 0b1110,
+            1241..=1320 => 0b1111,
+            _ => return Err(AxpError::InvalidCurrent(current_ma)),
+        };
+        // Map your ChargeTargetVoltage enum to the 2-bit value AXP192 expects for REG33H[6:5]
+        let target_voltage_bits = match target_voltage {
+            ChargeTargetVoltage::V4_10 => 0b00,
+            ChargeTargetVoltage::V4_15 => 0b01,
+            ChargeTargetVoltage::V4_20 => 0b10,
+            ChargeTargetVoltage::V4_36 => 0b11,
+        };
+
+        self.ll
+            .charge_control_1()
+            .write(|r| {
+                r.set_charge_enable(enable_charging);
+                r.set_charge_target_voltage_setting(target_voltage_bits);
+                r.set_end_charge_current_select(end_current_is_15_percent);
+                r.set_charge_current_setting(current_bits);
+            })
+            .await
+    }
+
+    #[bisync(async_suffix = "async")]
+    pub async fn set_exten_enable(&mut self, enable: bool) -> Result<(), AxpError<I2CBusErr>> {
+        self.ll
+            .dcdc_13_ldo_23_control()
+            .modify(|r| {
+                r.set_exten_enable_alt(enable);
+            })
+            .await
+    }
+
+    #[bisync(async_suffix = "async")]
+    pub async fn set_gpio0_ldo_mode_and_voltage(
+        &mut self,
+        voltage_mv: u16,
+    ) -> Result<(), AxpError<I2CBusErr>> {
+        // Assuming GpioMode::SpecialOutput010 is 0b010
+        self.ll
+            .gpio_0_control()
+            .modify(|r| r.set_mode(0b010))
+            .await?;
+
+        if !(1800..=3300).contains(&voltage_mv) {
+            return Err(AxpError::InvalidVoltage(voltage_mv));
+        }
+        let setting = ((voltage_mv.saturating_sub(1800)) / 100) as u8 & 0x0F;
+        self.ll
+            .gpio_0_ldoio_0_voltage()
+            .write(|r| {
+                r.set_voltage_setting(setting);
+            })
+            .await
+    }
+
+    #[bisync(async_suffix = "async")]
+    pub async fn configure_vbus_settings(
+        &mut self,
+        path_override_nvbusen: bool,
+        vhold_limit_disable: bool,
+        current_limit_disable: bool,
+    ) -> Result<(), AxpError<I2CBusErr>> {
+        self.ll
+            .vbus_ipsout_channel()
+            .write(|r| {
+                r.set_vbus_ipsout_path_override_nvbusen(path_override_nvbusen);
+                r.set_vbus_vhold_limit_enable(!vhold_limit_disable);
+                if vhold_limit_disable {
+                    r.set_vhold_voltage_setting(0b000); // 4.0V e.g.
+                } else {
+                    r.set_vhold_voltage_setting(0b100); // 4.4V default when enabled
+                }
+                r.set_vbus_current_limit_enable(!current_limit_disable);
+                r.set_vbus_current_limit_value_select(false); // 500mA if enabled
+            })
+            .await
+    }
+
+    #[bisync(async_suffix = "async")]
+    pub async fn set_battery_temp_protection_raw_n(
+        &mut self,
+        n_value_for_0x39: u8,
+    ) -> Result<(), AxpError<I2CBusErr>> {
+        self.ll
+            .battery_charge_high_temp()
+            .write(|r| {
+                r.set_threshold_n_setting(n_value_for_0x39);
+            })
+            .await
+    }
+
+    #[bisync(async_suffix = "async")]
+    pub async fn enable_backup_battery_charging(
+        &mut self,
+        enable: bool,
+        target_3v0: bool,
+        current_200ua: bool,
+    ) -> Result<(), AxpError<I2CBusErr>> {
+        let voltage_setting = if target_3v0 { 0b01 } else { 0b00 };
+        let current_setting = if current_200ua { 0b10 } else { 0b01 };
+        self.ll
+            .battery_charge_control()
+            .write(|r| {
+                r.set_backup_battery_charge_enable(enable);
+                r.set_backup_battery_target_voltage_setting(voltage_setting);
+                r.set_backup_battery_charge_current_setting(current_setting);
+            })
+            .await
+    }
+
+    #[bisync(async_suffix = "async")]
+    pub async fn configure_chgled_and_battery_detection(
+        &mut self,
+        battery_detection: bool,
+        n_oe_delay_2s: bool,
+    ) -> Result<(), AxpError<I2CBusErr>> {
+        self.ll
+            .shutdown_battery_chgled_control()
+            .modify(|r| {
+                r.set_battery_detection_enable(battery_detection);
+                r.set_chgled_pin_function_setting(0b00); // Hi-Z
+                r.set_chgled_pin_source_control(false); // By Charger
+                if n_oe_delay_2s {
+                    r.set_n_oe_low_to_high_shutdown_delay(0b10); // 2S
+                } else {
+                    r.set_n_oe_low_to_high_shutdown_delay(0b00); // 0.5S
+                }
+            })
+            .await
+    }
+    #[bisync(async_suffix = "async")]
+    pub async fn power_off_chip(&mut self) -> Result<(), AxpError<I2CBusErr>> {
+        self.ll
+            .shutdown_battery_chgled_control()
+            .modify(|r| {
+                r.set_mode_a_shutdown_control(true);
+            })
+            .await
     }
 }
